832138937e91dfcb5620a2b1cef41aec17907c03
Saksham Agrawal <31131966+sam14032000@users.noreply.github.com>
2021-04-11 20:42:31 +0530

    Update lex-pars.py

--- lex-pars.py
+++ lex-pars.py
@@ -1,5 +1,5 @@
 from rply import LexerGenerator
-
+from rply import ParserGenerator
 
 class Lexer():
     def __init__(self):
@@ -34,3 +34,43 @@ class Lexer():
     def get_lexer(self):
         self._add_tokens()
         return self.lexer.build()
+
+    
+    
+
+
+class Parser():
+    def __init__(self):
+        self.pg = ParserGenerator(
+            # A list of all token names accepted by the parser.
+            ['add', 'subtract', 'multiply', 'divide', 'left', 'right', 'up', 'down', 'assign', 'to', 'var', 'is', 'value', 'in', 'number', 'dot', 'comma'])
+
+    def parse(self):
+        @self.pg.production('program : PRINT OPEN_PAREN expression CLOSE_PAREN SEMI_COLON')
+        @self.pg.production('expression : expression SUM expression')
+        @self.pg.production('expression : expression SUB expression')
+        @self.pg.production('expression : NUMBER')
+        
+        def program(p):
+            return Print(p[2])
+
+        
+        def expression(p):
+            left = p[0]
+            right = p[2]
+            operator = p[1]
+            if operator.gettokentype() == 'SUM':
+                return Sum(left, right)
+            elif operator.gettokentype() == 'SUB':
+                return Sub(left, right)
+
+        
+        def number(p):
+            return Number(p[0].value)
+
+        @self.pg.error
+        def error_handle(token):
+            raise ValueError(token)
+
+    def get_parser(self):
+        return self.pg.build()
