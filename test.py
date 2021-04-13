3cc8d79ec39be50e66b4f6702c8bc3a3b4f5124b
Arshika Lalan <52136458+arshika77@users.noreply.github.com>
2021-04-13 14:27:06 +0530

    AST file

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from lexer import Lexer
 from parsetrans import Parser
 
 text_input = """
-ADD LEFT . ADDITION 5,7  8 amity     20, 12.
+ADD LEFT . ADDITION 5,7  8 amity     19var20haan.
 """
 
 lexer = Lexer().get_lexer()
